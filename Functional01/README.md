d86acaccb4d88283970b5bca384ce19c4e51c86a
eucarizan <eucarizan@gmail.com>
2024-04-05 17:06:31 +0800

    functional-1: moreY

--- Functional01/README.md
+++ Functional01/README.md
@@ -10,6 +10,7 @@ number|solution|test|description
 2|[square](src/main/java/Square.java)|[unit test](src/test/java/SquareTest.java)|Given a list of integers, return a list where each integer is multiplied with itself.
 3|[addStar](src/main/java/AddStar.java)|[unit test](src/test/java/AddStarTest.java)|Given a list of strings, return a list where each string has "*" added at its end.
 4|[copies3](src/main/java/Copies3.java)|[unit test](src/test/java/Copies3Test.java)|Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+5|[moreY](src/main/java/MoreY.java)|[unit test](src/test/java/MoreYTest.java)|desc
 <hr/>
 <!-- 0|[name](src/main/java)|[unit test](src/test/java)|desc-->
 
---
+++ Functional01/src/test/java/MoreYTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Arrays;
+
+@SuppressWarnings("unused")
+class MoreYTest {
+    @ParameterizedTest(name = "{0} -- moreY({1}) == {2}")
+    @DisplayName("moreY test")
+    @CsvSource(value = {
+            "test1 : a, b, c : [yay, yby, ycy]",
+            "test2 : hello, there : [yhelloy, ytherey]",
+            "test3 : yay : [yyayy]",
+            "test4 : a, xx : [yay, yxxy]",
+            "test5 : '' : []",
+            "test6 : xx, yy, zz : [yxxy, yyyy, yzzy]"
+    }, delimiter = ':')
+    public void test(String name, String input, String expected) {
+        List<String> param;
+
+        if (input.isEmpty()) {
+            param = List.of();
+        } else {
+            param= Arrays.stream(input.split(", ")).toList();
+        }
+
+        var result = MoreY.moreY(param).toString();
+
+        assertEquals(expected, result);
+    }
+}
